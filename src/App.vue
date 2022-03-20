3b3445138c1d46331e441b785d6bf0c59f358332
isaac-m-carter <270033249@yoobeestudent.ac.nz>
2022-03-21 10:53:04 +1300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,6 @@ import Keiran from './components/Keiran_Component.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <Isaac />
   </main>
