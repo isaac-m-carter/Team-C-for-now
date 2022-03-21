bcdcc412e319f6c985f4b8ab5532e428205f30f8
1001JL <85082315+1001JL@users.noreply.github.com>
2022-03-22 09:34:14 +1300

    Update Rubi_Component.vue

---
+++ src/components/Rubi_Component.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>bad weather</h3>
+    <img class="photo" src="https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2235&q=80" alt="">
+</template>
+<style scoped>
+ .photo{
+     border: 1px black solid;
+     margin: 10px;
+     height: 2fr;
+ }
+</style>
